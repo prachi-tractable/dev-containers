4f6c561dd28d955dff789002d5881041eda9033c
Harry Withers <harry.withers@tractable.ai>
2022-04-21 15:06:00 +0100

    test: add bucket check to health endpoint

---
+++ src/bucket.ts
@@ -0,0 +1,6 @@
+import { S3Client } from '@aws-sdk/client-s3'
+
+export const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    endpoint: process.env.AWS_ENDPOINT,
+})
--- src/health.ts
+++ src/health.ts
@@ -1,9 +1,17 @@
+import { ListObjectsCommand } from '@aws-sdk/client-s3'
 import express from 'express'
+import { s3Client } from './bucket'
 import { database } from './database'
 
 export const health: express.Handler = async (_, res, _next) => {
     try {
         await database.$executeRaw`SELECT 1;`
+
+        await s3Client.send(
+            new ListObjectsCommand({
+                Bucket: process.env.AWS_BUCKET,
+            })
+        )
     } catch (error) {
         return res.status(500).send('Not OK')
     }
